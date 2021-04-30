f207f93b807183b894a584234f5eadaaac0b94ce
Eero Hakio <eero.hakio@ambientia.fi>
2021-04-30 16:13:01 +0300

    KYV-79: Add timestamps to logs, add logs to totals service

--- packages/cart-experience-api/src/service/totalsService.ts
+++ packages/cart-experience-api/src/service/totalsService.ts
@@ -1,5 +1,6 @@
 import type { Cart, CartItem, CartTotals } from '@verkkokauppa/cart-backend'
 import { getPrice } from '@verkkokauppa/price-backend'
+import { logger } from '@verkkokauppa/core'
 
 interface CartItemRowTotal {
   netValue: number
@@ -22,6 +23,8 @@ type CartWithItemsAndTotals = Cart & {
 
 export const calculate = async (p: Cart): Promise<CartWithItemsAndTotals> => {
   const { cartId, items } = p
+  logger.debug(`Start calculate process for cartId: ${cartId}`)
+
   const cart = {
     ...p,
     items: await Promise.all(items.map((item) => calculateItem({ item }))),
@@ -34,9 +37,15 @@ export const calculateItem = async (p: {
   item: CartItem
 }): Promise<CartItemWithTotals> => {
   const { item } = p
+  logger.debug(
+    `Start calculate process for product: ${item.productId} in cart ${item.cartId}`
+  )
+
   const { productId, quantity } = item
   const price = await getPrice({ productId })
-
+  logger.debug(
+    `Price resolved for product: ${item.productId} in cart ${item.cartId}`
+  )
   const unitPrice = {
     netValue: parseFloat(price.original.netValue),
     grossValue: parseFloat(price.original.grossValue),
@@ -58,6 +67,8 @@ export const calculateCart = (p: {
   items: CartItemWithTotals[]
 }): CartTotals => {
   const { cartId, items } = p
+  logger.debug(`Combine totals for cartId: ${cartId}`)
+
   let netValue = 0
   let grossValue = 0
   let vatValue = 0
