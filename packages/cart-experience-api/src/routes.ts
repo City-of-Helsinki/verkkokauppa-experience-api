b273abfa1f1403744f19478c2cc81e17dd7339cf
Aleksi Jetsu <aleksi.jetsu@ambientia.fi>
2021-05-07 09:11:59 +0300

    KYV-85 Added controller for editing quantity of products in cart and added new route for edit

--- packages/cart-experience-api/src/routes.ts
+++ packages/cart-experience-api/src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import { CreateController } from './api/createController'
 import { GetController } from './api/getController'
 import { AddItemController } from './api/addItemController'
+import { EditItemController } from './api/editItemController'
 import { RemoveItemController } from './api/removeItemController'
 import { TotalsController } from './api/totalsController'
 const createController = new CreateController()
@@ -10,14 +11,18 @@ const getController = new GetController()
 const addItemController = new AddItemController()
 const removeItemController = new RemoveItemController()
 const totalsController = new TotalsController()
+const editItemController = new EditItemController()
+
 
 const router = Router()
-router.post('/', (req, res) => createController.execute(req, res))
-router.get('/:cartId', (req, res) => getController.execute(req, res))
-router.post('/:cartId/items', (req, res) => addItemController.execute(req, res))
+router.put('/:cartId/edit/items/:productId', (req, res) => editItemController.execute(req, res))
 router.delete('/:cartId/items/:productId', (req, res) =>
   removeItemController.execute(req, res)
 )
+router.post('/:cartId/items', (req, res) => addItemController.execute(req, res))
 router.get('/:cartId/totals', (req, res) => totalsController.execute(req, res))
+router.get('/:cartId', (req, res) => getController.execute(req, res))
+router.post('/', (req, res) => createController.execute(req, res))
 
 export default router
+
