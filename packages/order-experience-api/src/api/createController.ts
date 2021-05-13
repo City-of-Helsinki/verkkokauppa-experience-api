16bde34f0ad362e14bd506e1e4d88ebe724404f3
Eero Hakio <eero.hakio@ambientia.fi>
2021-05-13 15:30:20 +0300

    KYV-61: Add support to order cart, add support to create cart with items, fix tsconfig paths, cleanup lint issues

--- packages/order-experience-api/src/api/createController.ts
+++ packages/order-experience-api/src/api/createController.ts
@@ -1,10 +1,16 @@
 import { AbstractController, Data, logger } from '@verkkokauppa/core'
 import type { Request, Response } from 'express'
-import { createOrder} from '@verkkokauppa/order-backend'
+import { createOrder, createOrderWithItems } from '@verkkokauppa/order-backend'
 
 export class CreateController extends AbstractController {
   protected async implementation(req: Request, res: Response): Promise<any> {
-    return this.create(req, res)
+    const { items } = req.body
+
+    if (items && items.length > 0) {
+      return this.createWithItems(req, res)
+    } else {
+      return this.create(req, res)
+    }
   }
 
   protected async create(req: Request, res: Response): Promise<any> {
@@ -14,6 +20,35 @@ export class CreateController extends AbstractController {
 
     try {
       dto.data = await createOrder({ namespace, user })
+    } catch (error) {
+      logger.error(error)
+      if (error.response.status === 400) {
+        return this.clientError(res, 'Invalid request')
+      }
+      return this.fail(res, error.toString())
+    }
+    return this.created<any>(res, dto.serialize())
+  }
+
+  protected async createWithItems(req: Request, res: Response): Promise<any> {
+    const { namespace, user, items, customer } = req.body
+    if (customer === undefined) {
+      return this.clientError(res, 'Customer not specified')
+    }
+    if (namespace === undefined) {
+      return this.clientError(res, 'Namespace not specified')
+    }
+    const dto = new Data()
+    logger.debug(
+      `Create Order with Items for namespace ${namespace} and user ${user}`
+    )
+    try {
+      dto.data = await createOrderWithItems({
+        namespace,
+        user,
+        items,
+        customer,
+      })
     } catch (error) {
       logger.error(error)
       if (error.response.status === 400) {
