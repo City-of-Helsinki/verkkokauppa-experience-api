b06bada092370735e5427d966d473b15c2a39ceb
Eero Hakio <eero.hakio@ambientia.fi>
2021-05-14 15:01:58 +0300

    KYV-83: Fix order cancel to check if theres items for new cart

--- packages/order-experience-api/src/api/cancelController.ts
+++ packages/order-experience-api/src/api/cancelController.ts
@@ -1,7 +1,7 @@
 import { AbstractController, Data, logger } from '@verkkokauppa/core'
 import type { Request, Response } from 'express'
 import { cancelOrder } from '@verkkokauppa/order-backend'
-import { createCartWithItems } from '@verkkokauppa/cart-backend'
+import { createCart, createCartWithItems } from '@verkkokauppa/cart-backend'
 
 export class CancelController extends AbstractController {
   protected async implementation(req: Request, res: Response): Promise<any> {
@@ -14,12 +14,24 @@ export class CancelController extends AbstractController {
 
     try {
       const order = await cancelOrder({ orderId })
-      const cart = await createCartWithItems({
-        namespace: order.namespace,
-        user: order.user || '',
-        items: order.items,
-      })
-      dto.data = { order, cart }
+      if (order.items && order.items.length > 0) {
+        dto.data = {
+          order,
+          cart: await createCartWithItems({
+            namespace: order.namespace,
+            user: order.user || '',
+            items: order.items,
+          }),
+        }
+      } else {
+        dto.data = {
+          order,
+          cart: await createCart({
+            namespace: order.namespace,
+            user: order.user || '',
+          }),
+        }
+      }
     } catch (error) {
       logger.error(error)
       if (error.response.status === 400) {
