af7e72163bcd7f29efa892c1e6cdacb7f7dd6542
Eero Hakio <eero.hakio@ambientia.fi>
2021-05-17 09:56:58 +0300

    KYV-58: Add support to get order

--- packages/order-experience-api/src/routes.ts
+++ packages/order-experience-api/src/routes.ts
@@ -5,25 +5,29 @@ import { CartToOrder } from './api/cartToOrder'
 import { CancelController } from './api/cancelController'
 import { SetCustomerController } from './api/setCustomerController'
 import { AddItemController } from './api/addItemController'
+import { GetController } from './api/getController'
 
 const createController = new CreateController()
 const cartToOrderController = new CartToOrder()
 const cancelController = new CancelController()
 const setCustomerController = new SetCustomerController()
 const addItemController = new AddItemController()
+const getController = new GetController()
 
 const router = Router()
 router.post('/', (req, res) => createController.execute(req, res))
-router.post('/:orderId/customer', (req, res) => setCustomerController.execute(req, res))
-router.post('/:orderId/items', (req, res) => addItemController.execute(req, res))
+router.get('/:orderId', (req, res) => getController.execute(req, res))
+router.post('/:orderId/customer', (req, res) =>
+  setCustomerController.execute(req, res)
+)
+router.post('/:orderId/items', (req, res) =>
+  addItemController.execute(req, res)
+)
 router.post('/:orderId/cancel', (req, res) =>
   cancelController.execute(req, res)
 )
 router.post('/convert/:cartId', (req, res) =>
   cartToOrderController.execute(req, res)
 )
-
-
-
 
 export default router
