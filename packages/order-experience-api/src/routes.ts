879cdf3f6ae8c482d1ecb22011bb9949062b0b4b
Antti Ranta <antti.ranta@ambientia.fi>
2021-06-03 21:35:26 +0300

    KYV-57 Initial recurring order code for experience api

--- packages/order-experience-api/src/routes.ts
+++ packages/order-experience-api/src/routes.ts
@@ -1,11 +1,15 @@
-import { Router } from 'express'
+import {Router} from 'express'
 
-import { CreateController } from './api/createController'
-import { CartToOrder } from './api/cartToOrder'
-import { CancelController } from './api/cancelController'
-import { SetCustomerController } from './api/setCustomerController'
-import { AddItemController } from './api/addItemController'
-import { GetController } from './api/getController'
+import {CreateController} from './api/createController'
+import {CartToOrder} from './api/cartToOrder'
+import {CancelController} from './api/cancelController'
+import {SetCustomerController} from './api/setCustomerController'
+import {AddItemController} from './api/addItemController'
+import {GetController} from './api/getController'
+import {GetRecurringOrderController} from "./api/getRecurringOrder"
+import {SearchActiveRecurringOrderController} from "./api/searchActiveRecurringOrder"
+import {CreateRecurringOrderController} from "./api/createRecurringOrder"
+import {CreateRecurringOrdersFromOrderController} from "./api/createRecurringOrdersFromOrder"
 
 const createController = new CreateController()
 const cartToOrderController = new CartToOrder()
@@ -13,21 +17,35 @@ const cancelController = new CancelController()
 const setCustomerController = new SetCustomerController()
 const addItemController = new AddItemController()
 const getController = new GetController()
+const getRecurringOrderCtrl = new GetRecurringOrderController()
+const searchActiveRecurringOrderCtrl = new SearchActiveRecurringOrderController()
+const createRecurringOrderCtrl = new CreateRecurringOrderController()
+const createRecurringOrdersFromOrderCtrl = new CreateRecurringOrdersFromOrderController()
 
 const router = Router()
 router.post('/', (req, res) => createController.execute(req, res))
 router.get('/:orderId', (req, res) => getController.execute(req, res))
+router.get('/recurringorder/:id', (req, res) => getRecurringOrderCtrl.execute(req, res))
 router.post('/:orderId/customer', (req, res) =>
-  setCustomerController.execute(req, res)
+    setCustomerController.execute(req, res)
 )
 router.post('/:orderId/items', (req, res) =>
-  addItemController.execute(req, res)
+    addItemController.execute(req, res)
 )
 router.post('/:orderId/cancel', (req, res) =>
-  cancelController.execute(req, res)
+    cancelController.execute(req, res)
 )
 router.post('/convert/:cartId', (req, res) =>
-  cartToOrderController.execute(req, res)
+    cartToOrderController.execute(req, res)
+)
+router.post('/recurringorder/create', (req, res) =>
+    createRecurringOrderCtrl.execute(req, res)
+)
+router.post('/recurringorder/create-from-order', (req, res) =>
+    createRecurringOrdersFromOrderCtrl.execute(req, res)
+)
+router.post('/recurringorder/search/active', (req, res) =>
+    searchActiveRecurringOrderCtrl.execute(req, res)
 )
 
 export default router
