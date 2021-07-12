9af9a98c712d4e42228b1f4abe5a54ba0cc20aff
Antti Ranta <antti.ranta@ambientia.fi>
2021-07-12 14:07:39 +0300

    KYV-184 Added API for getting payment method list

---
+++ packages/payment-experience-api/src/api/getPaymentMethodListController.ts
@@ -0,0 +1,38 @@
+import { AbstractController, Data, logger } from '@verkkokauppa/core'
+import type { Request, Response } from 'express'
+import {getOrder, OrderItem} from '@verkkokauppa/order-backend'
+import { getPaymentMethodList } from '@verkkokauppa/payment-backend'
+
+export class GetPaymentMethodListController extends AbstractController {
+  protected async implementation(request: Request, result: Response): Promise<any> {
+    const { orderId } = request.params
+    if (orderId === undefined) {
+      return this.clientError(result, 'Order ID not specified')
+    }
+
+    const dto = new Data()
+    try {
+        const order = await getOrder({ orderId })
+        dto.data = await getPaymentMethodList({
+            request: {
+                namespace: order.namespace,
+                totalPrice: calculateTotalPrice(order.items) // TODO: we have to calculate totals here?!
+                // TODO: currency, where to get from?
+            }
+        })
+    } catch (error) {
+        logger.error(error)
+        return this.fail(result, error.toString())
+    }
+
+    return this.success<any>(result, dto.serialize())
+  }
+}
+
+const calculateTotalPrice = (items: OrderItem[]): number => {
+    let totalPrice = 0
+    for (const item of items) {
+        totalPrice += item.rowPriceTotal
+    }
+    return totalPrice
+}
