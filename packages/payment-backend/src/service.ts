6105035e7ab48e9a38417806816b007937fbb842
Eero Hakio <eero.hakio@ambientia.fi>
2021-07-30 11:14:04 +0300

    KYV-183: Change signature of get payment request to create payment

--- packages/payment-backend/src/service.ts
+++ packages/payment-backend/src/service.ts
@@ -1,12 +1,15 @@
 import axios from 'axios'
-import type {Order} from "./types"
+import type { Order } from './types'
 
 const PAYMENT_METHOD_MAP = new Map()
-    .set('invoice', 'billing')
-    .set('visma-pay', 'online')
+  .set('invoice', 'billing')
+  .set('visma-pay', 'online')
 
-export const getPaymentRequestData = async (parameters: {order: Order, paymentMethod: string}): Promise<string> => {
-  const {order, paymentMethod}  = parameters
+export const createPaymentFromOrder = async (parameters: {
+  order: Order
+  paymentMethod: string
+}): Promise<string> => {
+  const { order, paymentMethod } = parameters
   if (!process.env.PAYMENT_BACKEND_URL) {
     throw new Error('No payment API backend URL set')
   }
@@ -22,9 +25,7 @@ export const getPaymentRequestData = async (parameters: {order: Order, paymentMe
 
   // We use POST instead of GET since we need to send complex parameters,
   // although using GET would be semantically more correct.
-  const result = await axios.post<string>(
-    url, order
-  )
+  const result = await axios.post<string>(url, order)
 
   return result.data
 }
