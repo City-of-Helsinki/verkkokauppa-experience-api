6e6c6f795f3a1e42cd0861737d073bec845838bc
Eero Hakio <eero.hakio@ambientia.fi>
2021-08-03 09:15:25 +0300

    KYV-184: Prettier payment backend service

--- packages/payment-backend/src/service.ts
+++ packages/payment-backend/src/service.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import type { Order, PaymentMethod, PaymentMethodListRequest } from "./types"
+import type { Order, PaymentMethod, PaymentMethodListRequest } from './types'
 
 const PAYMENT_METHOD_MAP = new Map()
   .set('invoice', 'billing')
@@ -30,8 +30,10 @@ export const createPaymentFromOrder = async (parameters: {
   return result.data
 }
 
-export const getPaymentMethodList = async (parameters: {request: PaymentMethodListRequest}): Promise<PaymentMethod[]> => {
-  const {request}  = parameters
+export const getPaymentMethodList = async (parameters: {
+  request: PaymentMethodListRequest
+}): Promise<PaymentMethod[]> => {
+  const { request } = parameters
   if (!process.env.PAYMENT_BACKEND_URL) {
     throw new Error('No payment API backend URL set')
   }
@@ -40,8 +42,6 @@ export const getPaymentMethodList = async (parameters: {request: PaymentMethodLi
 
   // We use POST instead of GET since we need to send complex parameters,
   // although using GET would be semantically more correct.
-  const result = await axios.post<PaymentMethod[]>(
-      url, request
-  )
+  const result = await axios.post<PaymentMethod[]>(url, request)
   return result.data
 }
