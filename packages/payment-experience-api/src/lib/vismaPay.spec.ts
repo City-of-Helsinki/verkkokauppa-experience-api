18795c4d50e330b5329f024a3180a67f11701f14
Eero Hakio <eero.hakio@ambientia.fi>
2021-08-20 10:49:12 +0300

    KYV-95: Redirect user to summary instead of paymentmethod step when cancelling payment in gateway

--- packages/payment-experience-api/src/lib/vismaPay.spec.ts
+++ packages/payment-experience-api/src/lib/vismaPay.spec.ts
@@ -91,7 +91,7 @@ describe('Test User redirection creation', () => {
       vismaStatus: { canRetry: true, paymentPaid: false, valid: true },
     })
     expect(result.toString()).toBe(
-      `${process.env.REDIRECT_PAYMENT_URL_BASE}/145d8829-07b7-4b03-ab0e-24063958ab9b/paymentmethod?paymentPaid=false`
+      `${process.env.REDIRECT_PAYMENT_URL_BASE}/145d8829-07b7-4b03-ab0e-24063958ab9b/summary?paymentPaid=false`
     )
   })
   it('Should return service failure with retry if order is not paid and with service specific configuration set', async () => {
@@ -110,7 +110,7 @@ describe('Test User redirection creation', () => {
       vismaStatus: { canRetry: true, paymentPaid: false, valid: true },
     })
     expect(result.toString()).toBe(
-      `${configMock.configurationValue}/145d8829-07b7-4b03-ab0e-24063958ab9b/paymentmethod?paymentPaid=false`
+      `${configMock.configurationValue}/145d8829-07b7-4b03-ab0e-24063958ab9b/summary?paymentPaid=false`
     )
   })
   it('Should return checkout success if order is paid and no service specific configuration set', async () => {
