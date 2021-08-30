4699a449408f99fc97a268eaf409c8d3936bb9aa
Waltteri Pakalén <waltteri.pakalen@ambientia.fi>
2021-08-30 16:20:26 +0300

    KYV-144: Define errors

---
+++ packages/payment-backend/src/errors.ts
@@ -0,0 +1,77 @@
+import {
+  ExperienceError,
+  ExperienceFailure,
+  StatusCode,
+} from '@verkkokauppa/core'
+
+export class PaymentMethodValidationError extends ExperienceError {
+  constructor() {
+    super({
+      code: 'payment-method-validation-failed',
+      message:
+        'paymentMethod must be one of invoice, visma-pay, nordea, osuuspankki, creditcards',
+      responseStatus: StatusCode.BadRequest,
+      logLevel: 'debug',
+    })
+  }
+}
+
+export class CreatePaymentFromOrderFailure extends ExperienceFailure {
+  constructor(source: Error) {
+    super({
+      code: 'failed-to-create-payment-from-order',
+      message: 'Failed to create payment from order',
+      source,
+    })
+  }
+}
+
+export class GetPaymentMethodListFailure extends ExperienceFailure {
+  constructor(source: Error) {
+    super({
+      code: 'failed-to-get-payment-method-list',
+      message: 'Failed to get payment method list',
+      source,
+    })
+  }
+}
+
+export class CheckVismaReturnUrlFailure extends ExperienceFailure {
+  constructor(source: Error) {
+    super({
+      code: 'failed-to-check-visma-return-url',
+      message: 'Failed to check visma return url',
+      source,
+    })
+  }
+}
+
+export class GetPaymentUrlFailure extends ExperienceFailure {
+  constructor(source: Error) {
+    super({
+      code: 'failed-to-get-payment-url',
+      message: 'Failed to get payment url',
+      source,
+    })
+  }
+}
+
+export class GetPaymentStatusFailure extends ExperienceFailure {
+  constructor(source: Error) {
+    super({
+      code: 'failed-to-get-payment-status',
+      message: 'Failed to get payment status',
+      source,
+    })
+  }
+}
+
+export class GetPaymentForOrderFailure extends ExperienceFailure {
+  constructor(source: Error) {
+    super({
+      code: 'failed-to-get-payment-for-order',
+      message: 'Failed to get payment for order',
+      source,
+    })
+  }
+}
