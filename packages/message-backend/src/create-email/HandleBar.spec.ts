ccacf7a96d0167704f4f33fd83d47f2dfc1a3d20
severikupari1 <severikupari1@gmail.com>
2021-09-01 17:23:45 +0300

    KYV-138:
    Added footer as base64 encoded image and created more tests for template.

--- packages/message-backend/src/create-email/HandleBar.spec.ts
+++ packages/message-backend/src/create-email/HandleBar.spec.ts
@@ -1,94 +1,106 @@
 import { HandleBarTemplate } from "./service";
-import type { OrderConfirmationEmailParameters } from "./types";
+import type { Order, OrderConfirmationEmailParameters } from "./types";
 
-describe('Test Calculate Totals for Order', () => {
-    it('Should throw error with no backend url set', async () => {
+const fs = require('fs');
+describe('Create templates from parameters', () => {
+    it('Should compile orderConfirmation.hbs template to html and assert that it matches snapshot', async () => {
+
+        const order = {
+            orderId: "dummy-order-KYV-136",
+            createdAt: "2021-08-16T07:56:57.599811",
+            items: [
+                {
+                    productId: "productId2",
+                    productName: "Kiinteähintainen tuote",
+                    quantity: 1,
+                    unit: "1",
+                    rowPriceNet: "600",
+                    rowPriceVat: "144",
+                    rowPriceTotal: "744",
+                    vatPercentage: "24",
+                    priceNet: "300",
+                    priceVat: "72",
+                    priceGross: "372",
+                    orderId: "orderId",
+                    orderItemId: "orderItemId"
+                },
+                {
+                    productId: "productId1",
+                    productName: "Kiinteähintainen tuote",
+                    quantity: 1,
+                    unit: "1",
+                    rowPriceNet: "600",
+                    rowPriceVat: "144",
+                    rowPriceTotal: "744",
+                    vatPercentage: "24",
+                    priceNet: "300",
+                    priceVat: "72",
+                    priceGross: "372",
+                    orderId: "orderId",
+                    orderItemId: "orderItemId"
+                },
+            ],
+            merchant: {
+                merchantName: "Helsingin kaupunki",
+                merchantStreet: "Kaupunkiympäristö",
+                merchantZip: "00099",
+                merchantCity: "merchantCity",
+                merchantPostalAddress: "PL 58231 (Työpajankatu 8)",
+                merchantEmail: "merchantEmail",
+                merchantPhone: "merchantPhone",
+                merchantUrl: "https://www.google.com/",
+                merchantBusinessId: "0201256-6",
+                merchantCustomerService: "Kaupunkiympäristön asiakaspalvelu",
+            },
+            customer: {
+                firstName: "Essi",
+                lastName: "esimerkki",
+                email: "essi.esimerkki@gmail.com",
+                phone: "+358123456789",
+                address: "Esimerkkiosoite 1",
+                district: "123456 Esimerkkitoimipaikka"
+            },
+            status: "draft",
+            type: "order",
+            payment: {
+                paymentId: "f7a4fde4-1d3c-3f60-8bbe-7ed5977bb92b",
+                timestamp: "20210901-051844",
+                namespace: "asukaspysakointi",
+                orderId: "e8bcb47c-ed17-3f4b-ad1d-079001d9d2a3",
+                status: "payment_created",
+                paymentMethod: "nordea",
+                paymentType: "order",
+                totalExclTax: 1440,
+                total: 1488,
+                taxAmount: 48,
+                description: null,
+                additionalInfo: "{\"payment_method\": nordea}",
+                token: "427a38b2607b105de58c7dbda2d8ce2f6fcb31d6cc52f77b8818c0b5dcd503f5",
+                paymentUrl: "https://www.vismapay.com/pbwapi/token/427a38b2607b105de58c7dbda2d8ce2f6fcb31d6cc52f77b8818c0b5dcd503f5"
+            }
+        } as Order;
 
         const templateParams: OrderConfirmationEmailParameters = {
-            headingHeader: "Tilausvahvistus ja kuitti",
-            headingDetails:
-                {
-                    orderId: "",
-                    endDate: "",
-                    service: "",
-                    serviceProvider: "",
-                    serviceProviderUrl: "",
-                }
-            ,
-            receiptHeader: "Maksutiedot",
-            receiptDetails: [
-                {
-                    description: "Kiinteähintainen tuote",
-                    fieldValue: "10.50 €",
-                    bold: true,
-                },
-                {
-                    description: "Sis. alv (24%)",
-                    fieldValue: "2.52 €",
-                    className: "pb-1",
-                },
-                {
-                    description: "Maksettu yhteensä",
-                    fieldValue: "10.50 €",
-                },
-                {
-                    description: "Maksutapa",
-                    fieldValue: "DanskeBank",
-                },
-                {
-                    description: "Päivämäärä",
-                    fieldValue: "06.05.2021 13:55:42"
-                },
-            ],
-            orderHeader: "Tilaajan tiedot",
-            orderDetails: [
-                {
-                    description: "Essi esimerkki",
-                },
-                {
-                    description: "essi.esimerkki@gmail.com",
-                },
-                {
-                    description: "+358123456789",
-                    className: "pb-1",
-                },
-                {
-                    description: "Esimerkkiosoite 1",
-                },
-                {
-                    description: "123456 Esimerkkitoimipaikka",
-                },
-            ],
-            merchantHeader: "Myyjän tiedot",
-            merchantDetails: [
-                {
-                    description: "Helsingin kaupunki",
-                },
-                {
-                    description: "Kaupunkiympäristö",
-                    className: "pb-1",
-                },
-                {
-                    description: "Kaupunkiympäristön asiakaspalvelu",
-                },
-                {
-                    description: "PL 58231 (Työpajankatu 8)",
-                },
-                {
-                    description: "00099 HELSINGIN KAUPUNKI",
-                },
-                {
-                    description: "Y-tunnus 0201256-6",
-                },
-            ],
+            order: order
         };
 
-        const template = HandleBarTemplate<OrderConfirmationEmailParameters>("en");
+        const template = HandleBarTemplate<OrderConfirmationEmailParameters>("fi");
         template.setFileName("orderConfirmation");
         const compiledTemplate = template.createTemplate();
 
         const orderConfirmationTemplate = compiledTemplate(templateParams);
-        expect(orderConfirmationTemplate).toBe(null);
+        // Used to make developing faster when html can be "hot reloaded" from file.
+        const writeToFile = false;
+        if (writeToFile) {
+            try {
+                fs.writeFileSync('packages/message-backend/src/create-email/__snapshots__/orderConfirmation.html', orderConfirmationTemplate)
+                //file written successfully
+            } catch (err) {
+                console.error(err)
+            }
+        }
+
+        expect(orderConfirmationTemplate).toMatchSnapshot();
 
     })
 
