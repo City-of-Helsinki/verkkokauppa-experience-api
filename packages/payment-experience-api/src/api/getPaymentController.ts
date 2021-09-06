5fb90a46bbd9be59c0992468898eee8599bde66a
Waltteri Pakalén <waltteri.pakalen@ambientia.fi>
2021-09-06 09:53:30 +0300

    KYV-199: add user validation to creating payment, add errors to payment-backend

--- packages/payment-experience-api/src/api/getPaymentController.ts
+++ packages/payment-experience-api/src/api/getPaymentController.ts
@@ -1,12 +1,13 @@
 import { AbstractController, Data, ValidatedRequest } from '@verkkokauppa/core'
 import type { Response } from 'express'
-import { getPaymentForOrder } from '@verkkokauppa/payment-backend'
+import { getPaymentAndValidateUserForOrder } from '@verkkokauppa/payment-backend'
 import { getOrder } from '@verkkokauppa/order-backend'
 import * as yup from 'yup'
 
 const requestSchema = yup.object().shape({
   params: yup.object().shape({
     orderId: yup.string().required(),
+    user: yup.string().required(),
   }),
 })
 
@@ -19,11 +20,13 @@ export class GetPaymentController extends AbstractController<
     result: Response
   ): Promise<any> {
     const {
-      params: { orderId },
+      params: { orderId, user },
     } = request
 
     const order = await getOrder({ orderId })
-    const dto = new Data(await getPaymentForOrder(order))
+    const dto = new Data(
+      await getPaymentAndValidateUserForOrder({ ...order, user })
+    )
 
     return this.success<any>(result, dto.serialize())
   }
