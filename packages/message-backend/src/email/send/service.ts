4be988df7470781639e1d07c28243a5fb214f77f
severikupari1 <severikupari1@gmail.com>
2021-09-14 11:24:14 +0300

    KYV-188 Added missing files
    Signed-off-by: severikupari1 <severikupari1@gmail.com>

---
+++ packages/message-backend/src/email/send/service.ts
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import { SendOrderConfirmationEmailFailure } from '../../errors'
+import { createOrderConfirmationEmailTemplate } from '../create/service'
+import type {
+  HbsTemplateFiles,
+  Order,
+  OrderConfirmationEmailParameters,
+} from '../create/types'
+
+function isMessageBackendUrlSet() {
+  if (!process.env.MESSAGE_BACKEND_URL) {
+    throw new Error('No message backend URL set')
+  }
+}
+
+export const sendEmailToCustomer = async (p: {
+  order: Order
+  fileName: HbsTemplateFiles
+  sendTo: string
+  emailHeader: string
+}): Promise<any> => {
+  isMessageBackendUrlSet()
+  const { order, fileName } = p
+
+  const created = await createOrderConfirmationEmailTemplate<OrderConfirmationEmailParameters>(
+    {
+      fileName: fileName,
+      templateParams: { order: order },
+    }
+  )
+
+  const url = `${process.env.MESSSAGE_BACKEND_URL}message/send/email`
+
+  try {
+    const result = await axios.post<any>(url, {
+      orderId: order.orderId,
+      receiver: p.sendTo,
+      header: p.emailHeader,
+      body: created.template,
+    })
+    return result.data
+  } catch (e) {
+    throw new SendOrderConfirmationEmailFailure(e)
+  }
+}
