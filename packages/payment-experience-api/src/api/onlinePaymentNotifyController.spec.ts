44c7cdf86ff61eec6d86463b3a71050c3fa974af
severikupari1 <severikupari1@gmail.com>
2021-09-15 10:20:44 +0300

    äAdded test

---
+++ packages/payment-experience-api/src/api/onlinePaymentNotifyController.spec.ts
@@ -0,0 +1,110 @@
+import axios from 'axios'
+import { OnlinePaymentNotifyController } from './onlinePaymentNotifyController'
+
+jest.mock('axios')
+const axiosMock = axios as jest.Mocked<typeof axios>
+
+const orderMock = {
+  orderId: '145d8829-07b7-4b03-ab0e-24063958ab9b',
+  namespace: 'test',
+  user: 'test@test.dev.hel',
+  createdAt: '1619157868',
+  type: 'order',
+  items: [
+    {
+      orderId: '145d8829-07b7-4b03-ab0e-24063958ab9b',
+      orderItemId: '19699acf-b0a3-440f-818f-e582825fa3a7',
+      productId: '30a245ed-5fca-4fcf-8b2a-cdf1ce6fca0d',
+      quantity: 1,
+      productName: 'Product Name',
+      unit: 'pcs',
+      rowPriceNet: '100',
+      rowPriceVat: '24',
+      rowPriceTotal: '124',
+      priceVat: '24',
+      priceNet: '100',
+      priceGross: '124',
+      vatPercentage: '24',
+    },
+  ],
+  merchant: {
+    merchantName: 'merchantName',
+    merchantStreet: 'merchantStreet',
+    merchantZip: 'merchantZip',
+    merchantCity: 'merchantCity',
+    merchantEmail: 'merchantEmail',
+    merchantPhone: 'merchantPhone',
+    merchantUrl: 'merchantUrl',
+    merchantBusinessId: 'merchantBusinessId',
+  },
+  customer: {
+    firstName: 'Essi',
+    lastName: 'esimerkki',
+    email: 'essi.esimerkki@gmail.com',
+    phone: '+358123456789',
+    address: 'Esimerkkiosoite 1',
+    district: '123456 Esimerkkitoimipaikka',
+  },
+}
+
+describe('Test receipt send', () => {
+  it('Receipt email send returns no errors', async () => {
+    process.env.REDIRECT_PAYMENT_URL_BASE = 'https://test.dev.hel'
+    process.env.CONFIGURATION_BACKEND_URL = 'https://test.dev.hel'
+    process.env.MESSAGE_BACKEND_URL = 'https://test.dev.hel'
+    process.env.PAYMENT_BACKEND_URL = 'https://test.dev.hel'
+
+    const configMock = {
+      configurationId: '2f815a93-4c5c-442f-ba09-f294ecc12679',
+      namespace: 'test',
+      configurationKey: 'ORDER_CREATED_REDIRECT_URL',
+      configurationValue: 'https://service.dev.hel',
+      restricted: false,
+    }
+
+    const paymentMock = {
+      paymentId: 'dummy-payment',
+      namespace: 'asukaspysakointi',
+      orderId: 'dummy-order',
+      status: 'payment_created',
+      paymentMethod: 'nordea',
+      paymentType: 'order',
+      totalExclTax: 200,
+      total: 248,
+      taxAmount: 48,
+      description: null,
+      additionalInfo: '{"payment_method": nordea}',
+      token: '427a38b2607b105de58c7dbda2d8ce2f6fcb31d6cc52f77b8818c0b5dcd503f5',
+      paymentMethodLabel: 'paymentMethodLabel',
+    }
+
+    const messageMock = {
+      template: 'template',
+      error: '',
+    }
+
+    axiosMock.get.mockImplementation((url) => {
+      if (url.includes(`/public/get`)) {
+        return Promise.resolve({ data: configMock })
+      }
+      if (url.includes(`/payment/online/get`)) {
+        return Promise.resolve({ data: paymentMock })
+      }
+      return Promise.resolve({})
+    })
+
+    axiosMock.post.mockImplementation((url, data?: any) => {
+      expect(data.header).toEqual('emailHeader')
+      expect(data.orderId).toEqual('145d8829-07b7-4b03-ab0e-24063958ab9b')
+      expect(data.receiver).toEqual('essi.esimerkki@gmail.com')
+      if (url.includes(`message/send/email`)) {
+        return Promise.resolve({ data: messageMock })
+      }
+      return Promise.resolve({})
+    })
+
+    const result = await OnlinePaymentNotifyController.sendReceipt(orderMock)
+    expect(result.error).toBe(``)
+    expect(result.template).toBe('template')
+  })
+})
