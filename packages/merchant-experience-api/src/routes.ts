692e16efa63c8cd8306b13ddab23fc96a09397f1
Waltteri Pakalén <waltteri.pakalen@ambientia.fi>
2021-09-16 12:45:55 +0300

    Add health endpoints to experience-apis

--- packages/merchant-experience-api/src/routes.ts
+++ packages/merchant-experience-api/src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import { GetController } from './api/getController'
+import { Health } from './api/health'
 
 const router = Router()
 
 const getController = new GetController()
+const healthController = new Health()
 
+router.get('/health', (req, res) => healthController.execute(req, res))
 router.get('/:namespace', (req, res) => getController.execute(req, res))
 
 export default router
