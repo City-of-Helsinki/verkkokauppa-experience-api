ea578974c007dd38cd24429c3b5856cc2a9c6a8d
severikupari1 <severikupari1@gmail.com>
2022-08-11 09:35:19 +0300

    Created new endpoint to update all merchants

--- packages/merchant-experience-api/src/routes.ts
+++ packages/merchant-experience-api/src/routes.ts
@@ -3,6 +3,7 @@ import { GetController } from './api/getController'
 import { Health } from './api/health'
 import { Onboarding } from './api/onboarding'
 import { CreateMerchantController } from './api/createMerchantController'
+import { UpdateAllMerchantController } from './api/updateAllMerchantController'
 
 const router = Router()
 
@@ -10,6 +11,7 @@ const getController = new GetController()
 const healthController = new Health()
 const onboardingController = new Onboarding()
 const createMerchantController = new CreateMerchantController()
+const updateAllMerchantController = new UpdateAllMerchantController()
 
 router.get('/health', (req, res) => healthController.execute(req, res))
 router.get('/:namespace', (req, res) => getController.execute(req, res))
@@ -17,6 +19,10 @@ router.post('/:namespace', (req, res) => onboardingController.execute(req, res))
 
 router.post('/create/:namespace', (req, res) =>
   createMerchantController.execute(req, res)
+)
+
+router.post('/update/all/:namespace', (req, res) =>
+  updateAllMerchantController.execute(req, res)
 )
 
 export default router
