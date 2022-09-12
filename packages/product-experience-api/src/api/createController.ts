c6894a5ee7fef627069c9d3653544c99bfe0d09a
martin.lehtomaa <martin.lehtomaa@ambientia.fi>
2022-09-12 21:45:04 +0300

    [KYV-715] Added merchantId to product-mapping and order-item model. MerchantId for order-item is resolved from corresponding product-mapping.

--- packages/product-experience-api/src/api/createController.ts
+++ packages/product-experience-api/src/api/createController.ts
@@ -1,18 +1,23 @@
 import {
   AbstractController,
   Data,
+  ExperienceFailure,
   logger,
   ValidatedRequest,
 } from '@verkkokauppa/core'
 import type { Response } from 'express'
 import { createProductMapping } from '@verkkokauppa/product-mapping-backend'
 import * as yup from 'yup'
-import { validateApiKey } from '@verkkokauppa/configuration-backend'
+import {
+  validateApiKey,
+  getMerchantModels,
+} from '@verkkokauppa/configuration-backend'
 
 const requestSchema = yup.object().shape({
   body: yup.object().shape({
     namespace: yup.string().required(),
     namespaceEntityId: yup.string().required(),
+    merchantId: yup.string(),
   }),
   headers: yup.object().shape({
     'api-key': yup.string().required(),
@@ -27,7 +32,7 @@ export class CreateController extends AbstractController<typeof requestSchema> {
     res: Response
   ): Promise<any> {
     const {
-      body: { namespace, namespaceEntityId },
+      body: { namespace, namespaceEntityId, merchantId },
       headers: { 'api-key': apiKey },
     } = req
 
@@ -37,9 +42,36 @@ export class CreateController extends AbstractController<typeof requestSchema> {
       `Create product mapping for namespace: ${namespace} and product ${namespaceEntityId}`
     )
 
-    const dto = new Data(
-      await createProductMapping({ namespace, namespaceEntityId })
-    )
+    let dto
+    if (merchantId) {
+      dto = new Data(
+        await createProductMapping({ namespace, namespaceEntityId, merchantId })
+      )
+    } else {
+      const merchants = await getMerchantModels(namespace)
+      if (merchants && merchants.length === 1 && merchants[0]?.merchantId) {
+        dto = new Data(
+          await createProductMapping({
+            namespace,
+            namespaceEntityId,
+            merchantId: merchants[0].merchantId,
+          })
+        )
+      } else if (merchants && merchants.length > 1) {
+        throw new ExperienceFailure({
+          code: 'failed-to-create-product-mapping',
+          message:
+            'Failed to create product mapping - Could not resolve merchant, multiple found.',
+          source: new Error('merchantsByNamespace.length > 1'),
+        })
+      } else {
+        throw new ExperienceFailure({
+          code: 'failed-to-create-product-mapping',
+          message: 'Failed to create product mapping - Merchant not found.',
+          source: new Error('merchantsByNamespace.length <= 0'),
+        })
+      }
+    }
 
     return this.created<any>(res, dto.serialize())
   }
