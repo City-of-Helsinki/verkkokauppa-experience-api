1dfe51a2cce71490425da9f1635f53f8364470c8
severikupari1 <severikupari1@gmail.com>
2022-10-20 10:16:10 +0300

    Saving

---
+++ packages/payment-experience-api/src/api/paytrailOnlinePaymentReturnController.ts
@@ -0,0 +1,136 @@
+import { AbstractController, logger } from '@verkkokauppa/core'
+import type { Request, Response } from 'express'
+import {
+  createUserRedirectUrl,
+  isAuthorized,
+  isCardRenewal,
+  parseOrderIdFromRedirect,
+} from '../lib/vismaPay'
+import { URL } from 'url'
+import { getOrderAdmin } from '@verkkokauppa/order-backend'
+import {
+  cancelPaymentAdmin,
+  checkVismaReturnUrl,
+  getPaymentsForOrderAdmin,
+  Order,
+  PaymentStatus,
+} from '@verkkokauppa/payment-backend'
+import { sendReceiptToCustomer } from '../lib/sendEmail'
+
+export class PaytrailOnlinePaymentReturnController extends AbstractController {
+  protected readonly requestSchema = null
+
+  protected async implementation(
+    request: Request,
+    result: Response
+  ): Promise<any> {
+    const { query } = request
+    if (!process.env.REDIRECT_PAYMENT_URL_BASE) {
+      throw new Error('No default redirect url specified')
+    }
+    const orderId = parseOrderIdFromRedirect({ query })
+
+    if (!orderId) {
+      logger.error('No orderId specified redirect to general failure url')
+      return result.redirect(
+        302,
+        PaytrailOnlinePaymentReturnController.getFailureRedirectUrl().toString()
+      )
+    }
+
+    try {
+      const vismaStatus = await checkVismaReturnUrl({ params: query })
+      logger.debug(
+        `VismaStatus for order ${orderId}: ${JSON.stringify(vismaStatus)}`
+      )
+      if (!vismaStatus.valid) {
+        logger.debug(
+          `VismaStatus is not valid for ${orderId}, redirect to failure url`
+        )
+        return result.redirect(
+          302,
+          PaytrailOnlinePaymentReturnController.getFailureRedirectUrl().toString()
+        )
+      }
+
+      const order = await getOrderAdmin({ orderId })
+      const redirectUrl = await createUserRedirectUrl({
+        order,
+        vismaStatus,
+      })
+      // Function contains internal checks when to send receipt.
+      await sendReceiptToCustomer(vismaStatus, orderId, order)
+
+      // Only cancel authorized card renewals.
+      if (isAuthorized(vismaStatus) && isCardRenewal(vismaStatus)) {
+        const cancelled = await this.cancelAuthorizationPayments(order)
+        // Error count more than 0
+        let cancelledCount = cancelled[1]
+        if (cancelledCount != 0) {
+          return result.redirect(
+            302,
+            PaytrailOnlinePaymentReturnController.getCardRenewalFailureRedirectUrl(
+              order.orderId
+            ).toString()
+          )
+        }
+      }
+
+      return result.redirect(302, redirectUrl.toString())
+    } catch (error) {
+      logger.error(error)
+      logger.debug(
+        `Error occurred, redirect user to failure url for order ${orderId}`
+      )
+      return result.redirect(
+        302,
+        PaytrailOnlinePaymentReturnController.getFailureRedirectUrl().toString()
+      )
+    }
+  }
+
+  private static getFailureRedirectUrl() {
+    const redirectUrl = this.checkAndCreateRedirectUrl()
+    redirectUrl.pathname = 'failure'
+
+    return redirectUrl.toString()
+  }
+
+  public static checkAndCreateRedirectUrl() {
+    if (!process.env.REDIRECT_PAYMENT_URL_BASE) {
+      throw new Error('No default redirect url specified')
+    }
+    return new URL(process.env.REDIRECT_PAYMENT_URL_BASE)
+  }
+
+  private static getCardRenewalFailureRedirectUrl(orderId: string) {
+    const redirectUrl = this.checkAndCreateRedirectUrl()
+    redirectUrl.pathname = `${orderId}/card-update-failed`
+
+    return redirectUrl.toString()
+  }
+
+  public async cancelAuthorizationPayments(order: Order) {
+    let failCount = 0
+    let successCount = 0
+    const cancellablePayments = await getPaymentsForOrderAdmin(
+      order,
+      PaymentStatus.AUTHORIZED
+    )
+    for (const [, payment] of Object.entries(cancellablePayments)) {
+      const result = await cancelPaymentAdmin(payment.paymentId)
+      if (result.result === 0) {
+        logger.info(
+          `Payment cancellation successfully with payment id : ${payment.paymentId} `
+        )
+        successCount++
+      } else {
+        logger.info(
+          `Payment cancellation with payment id : ${payment.paymentId} failed`
+        )
+        failCount++
+      }
+    }
+    return [successCount, failCount]
+  }
+}
