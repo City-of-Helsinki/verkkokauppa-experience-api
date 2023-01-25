066fa3663d60dd81eaca897fb50f446d87f8fc6e
martin.lehtomaa <martin.lehtomaa@ambientia.fi>
2023-01-25 14:17:51 +0200

    [KYV-787] Implemented instant refund endpoint to payment-exp-api.

--- packages/payment-experience-api/src/routes.ts
+++ packages/payment-experience-api/src/routes.ts
@@ -14,6 +14,7 @@ import { PaytrailOnlinePaymentReturnController } from './api/paytrailOnlinePayme
 import { PaytrailOnlinePaymentNotifyController } from './api/paytrailOnlinePaymentNotifyController'
 import { PaytrailOnlineRefundPaymentSuccessController } from './api/paytrailOnlineRefundPaymentSuccessController'
 import { GetCardFormParametersController } from './api/getCardFormParametersController'
+import { CreateRefundController } from './api/createRefundController'
 
 const createPaymentController = new (withAuthentication(
   CreatePaymentController
@@ -34,6 +35,9 @@ const getPaymentController = new (withAuthentication(GetPaymentController))()
 const getPaymentAdminController = new GetPaymentAdminController()
 const healthController = new Health()
 const subscriptionTokenizeController = new SubscriptionTokenizeController()
+const instantRefundController = new CreateRefundController({
+  confirmAndCreatePayment: true,
+})
 
 const router = Router()
 router.get('/health', (req, res) => healthController.execute(req, res))
@@ -71,6 +75,10 @@ router.get('/subscription/:subscriptionId/tokenize', (req, res) =>
 
 router.get('/send/receipt/:orderId', (req, res) =>
   sendReceiptPaymentAdminInternalController.execute(req, res)
+)
+
+router.post('/refund/instant/:orderId', (req, res) =>
+  instantRefundController.execute(req, res)
 )
 
 export default router
