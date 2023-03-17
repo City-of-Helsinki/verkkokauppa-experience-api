2bdd4e07ff02ddd045f4cdb3a77c1ca19fd9e698
severikupari1 <severikupari1@gmail.com>
2023-03-17 09:40:16 +0200

    Added https support to localhost to work with paytrail api in local

--- packages/payment-experience-api/src/app.ts
+++ packages/payment-experience-api/src/app.ts
@@ -8,6 +8,16 @@ import * as yaml from 'yamljs'
 import * as path from 'path'
 const openapiDocument = yaml.load(path.join(__dirname, `/openapi.yaml`))
 
+// Importing the fs and https modules -------------- STEP 1
+import https from 'https'
+import fs from 'fs'
+
+// Read the certificate and the private key for the https server options
+// ------------------- STEP 2
+const options = {
+  key: fs.readFileSync(path.join(__dirname, `/config/cert.key`)),
+  cert: fs.readFileSync(path.join(__dirname, `/config/cert.crt`)),
+}
 const app = express()
 
 app.use(compression())
@@ -20,5 +30,11 @@ app.use(
   swaggerUi.serve,
   swaggerUi.setup(openapiDocument)
 )
+
+// Create the https server by initializing it with 'options'
+// -------------------- STEP 3
+https.createServer(options, app).listen(8081, () => {
+  console.log(`HTTPS server started on port 8081`)
+})
 
 export default app
