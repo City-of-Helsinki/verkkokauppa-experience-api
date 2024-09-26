1bf70c3a7867afaf97387aa3c083077611145627
severikupari1 <severikupari1@gmail.com>
2024-09-26 12:18:20 +0300

    Added get endpoint for accounting

--- packages/product-experience-api/src/routes.ts
+++ packages/product-experience-api/src/routes.ts
@@ -5,11 +5,13 @@ import { CreateController } from './api/createController'
 import { GetMappingController } from './api/getMappingController'
 import { CreateProductAccountingController } from './api/createProductAccountingController'
 import { Health } from './api/health'
+import { GetProductAccountingController } from './api/getProductAccountingController'
 
 const getController = new GetController()
 const createController = new CreateController()
 const getMappingController = new GetMappingController()
 const createProductAccountingController = new CreateProductAccountingController()
+const getProductAccountingController = new GetProductAccountingController()
 const healthController = new Health()
 
 const router = Router()
@@ -22,4 +24,9 @@ router.post('/', (req, res) => createController.execute(req, res))
 router.post('/:productId/accounting', (req, res) =>
   createProductAccountingController.execute(req, res)
 )
+
+router.get('/:productId/accounting', (req, res) =>
+  getProductAccountingController.execute(req, res)
+)
+
 export default router
