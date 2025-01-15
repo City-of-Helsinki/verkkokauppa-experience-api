49ae4c05c9e006890809cb69f25b4462a7698bdf
Severi Kupari <severikupari1@gmail.com>
2025-01-15 09:35:12 +0200

    Update HandleBar.spec.ts

--- packages/message-backend/src/email/create/HandleBar.spec.ts
+++ packages/message-backend/src/email/create/HandleBar.spec.ts
@@ -318,7 +318,7 @@ describe('Create templates from parameters', () => {
 
     const orderConfirmationTemplate = compiledTemplate(templateParams)
     // Used to make developing faster when html can be "hot reloaded" from file.
-    const writeToFile = true
+    const writeToFile = false
     if (writeToFile) {
       try {
         fs.writeFileSync(
