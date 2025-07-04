ba92ac818fc6e9e3eb54c8af2ba585a83d0ba134
Jarkko Laukkanen <jarkko.laukkanen@ambientia.fi>
2025-07-04 09:05:59 +0300

    [KYV-1209] added check if can cancel before cancelling order

--- packages/order-experience-api/src/api/cancelController.ts
+++ packages/order-experience-api/src/api/cancelController.ts
@@ -5,7 +5,7 @@ import {
   ValidatedRequest,
 } from '@verkkokauppa/core'
 import type { Response } from 'express'
-import { cancelOrder } from '@verkkokauppa/order-backend'
+import { cancelOrder, checkCanCancelOrder } from '@verkkokauppa/order-backend'
 import { getPublicServiceConfiguration } from '@verkkokauppa/configuration-backend'
 import { URL } from 'url'
 import * as yup from 'yup'
@@ -31,8 +31,18 @@ export class CancelController extends AbstractController<typeof requestSchema> {
       headers: { user },
     } = req
 
-    logger.debug(`Cancel Order ${orderId}`)
+    logger.debug(`Check if we can cancel the order ${orderId}`)
+    const canCancel = checkCanCancelOrder({ orderId })
 
+    if (!canCancel) {
+      // nothing to cancel
+      return this.respond(
+        res,
+        403,
+        'Can not cancel. If order is invoice could be canceller or invoiced already'
+      )
+    }
+    logger.debug(`Cancel Order ${orderId}`)
     const order = await cancelOrder({ orderId, user })
     const cancelUrl = await CancelController.createCancelUrl(order)
 
