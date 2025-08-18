771b8a60cca3097b7b08d1965750622ee8df9c13
Jarkko Laukkanen <jarkko.laukkanen@ambientia.fi>
2025-08-18 14:22:21 +0300

    [KYV-1272] added validations for invoice information, tests completed

--- packages/payment-experience-api/src/api/invoicingRedirectController.ts
+++ packages/payment-experience-api/src/api/invoicingRedirectController.ts
@@ -170,10 +170,31 @@ export class InvoicingRedirectController extends AbstractController {
                 quantity: yup.number().required(),
                 unit: yup.string().required(),
                 priceNet: yup.string().required(),
-                internalOrder: yup.string().notRequired(),
-                profitCenter: yup.string().notRequired(),
+                internalOrder: yup.string().nullable().default(''),
+                profitCenter: yup.string().nullable().default(''),
                 project: yup.string().nullable().default(''),
                 operationArea: yup.string().nullable().default(''),
+              })
+              .test((value) => {
+                if (!value) return false
+
+                // only internalOrder or profitCenter should be used
+                // if neither of them is used then project should be defined
+                const hasInternalOrder = !!value.internalOrder
+                const hasProfitCenter = !!value.profitCenter
+                const hasProject = !!value.project
+
+                // InternalOrder and ProfitCenter should not be provided at the same time
+                if (hasInternalOrder && hasProfitCenter) {
+                  return false
+                }
+
+                // if neither provided then project is required
+                if (!hasInternalOrder && !hasProfitCenter) {
+                  return hasProject
+                }
+
+                return true
               })
               .validateSync({
                 orderId: order.orderId,
@@ -194,9 +215,9 @@ export class InvoicingRedirectController extends AbstractController {
                 quantity: item.quantity,
                 unit: item.unit,
                 priceNet: item.priceNet,
-                internalOrder: productAccounting.internalOrder,
-                profitCenter: productAccounting.profitCenter,
-                project: productAccounting.project,
+                internalOrder: productAccounting?.internalOrder,
+                profitCenter: productAccounting?.profitCenter,
+                project: productAccounting?.project,
                 operationArea: productAccounting.operationArea,
               })
           }),
