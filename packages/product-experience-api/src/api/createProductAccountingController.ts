d6e5b3803e0b2fd78b44fe01d5afb1646f90293e
Jarkko Laukkanen <jarkko.laukkanen@ambientia.fi>
2025-08-18 16:01:34 +0300

    [KYV-1272] updated validations and validation message

--- packages/product-experience-api/src/api/createProductAccountingController.ts
+++ packages/product-experience-api/src/api/createProductAccountingController.ts
@@ -23,6 +23,31 @@ const nextEntitySchema = yup
     project: yup.string().nullable().notRequired(),
     operationArea: yup.string().nullable().notRequired(),
   })
+  .test(
+    'Only one accounting identifier',
+    'Accounting information should only have internalOrder or profitCenter defined. If neither is given then project has to be defined.',
+    (value) => {
+      if (!value) return false
+
+      // only internalOrder or profitCenter should be used
+      // if neither of them is used then project should be defined
+      const hasInternalOrder = !!value.internalOrder
+      const hasProfitCenter = !!value.profitCenter
+      const hasProject = !!value.project
+
+      // InternalOrder and ProfitCenter should not be provided at the same time
+      if (hasInternalOrder && hasProfitCenter) {
+        return false
+      }
+
+      // if neither provided then project is required
+      if (!hasInternalOrder && !hasProfitCenter) {
+        return hasProject
+      }
+
+      return true
+    }
+  )
   .nullable()
   .notRequired()
 
@@ -30,33 +55,59 @@ const requestSchema = yup.object().shape({
   params: yup.object().shape({
     productId: yup.string().required(),
   }),
-  body: yup.object({
-    vatCode: yup.string().required(),
-    companyCode: yup.string().required(),
-    mainLedgerAccount: yup.string().required(),
-    internalOrder: yup.string().notRequired(),
-    profitCenter: yup.string().notRequired(),
-    balanceProfitCenter: yup.string().required(),
-    project: yup.string().notRequired(),
-    operationArea: yup.string().notRequired(),
-    productInvoicing: yup
-      .object({
-        salesOrg: yup.string().required().length(4),
-        salesOffice: yup.string().required().length(4),
-        material: yup.string().required().length(8),
-        orderType: yup.string().required().length(4),
-      })
-      .notRequired()
-      .default(undefined),
-    activeFrom: yup.date().nullable().notRequired(),
-    nextEntity: nextEntitySchema.when('activeFrom', {
-      is: (value: null | undefined) => value !== null && value !== undefined,
-      then: yup
-        .object()
-        .required('nextEntity is required when activeFrom is provided'),
-      otherwise: nextEntitySchema,
-    }),
-  }),
+  body: yup
+    .object({
+      vatCode: yup.string().required(),
+      companyCode: yup.string().required(),
+      mainLedgerAccount: yup.string().required(),
+      internalOrder: yup.string().notRequired(),
+      profitCenter: yup.string().notRequired(),
+      balanceProfitCenter: yup.string().required(),
+      project: yup.string().notRequired(),
+      operationArea: yup.string().notRequired(),
+      productInvoicing: yup
+        .object({
+          salesOrg: yup.string().required().length(4),
+          salesOffice: yup.string().required().length(4),
+          material: yup.string().required().length(8),
+          orderType: yup.string().required().length(4),
+        })
+        .notRequired()
+        .default(undefined),
+      activeFrom: yup.date().nullable().notRequired(),
+      nextEntity: nextEntitySchema.when('activeFrom', {
+        is: (value: null | undefined) => value !== null && value !== undefined,
+        then: yup
+          .object()
+          .required('nextEntity is required when activeFrom is provided'),
+        otherwise: nextEntitySchema,
+      }),
+    })
+    .test(
+      'Only one accounting identifier',
+      'Accounting information should only have internalOrder or profitCenter defined. If neither is given then project has to be defined.',
+      (value) => {
+        if (!value) return false
+
+        // only internalOrder or profitCenter should be used
+        // if neither of them is used then project should be defined
+        const hasInternalOrder = !!value.internalOrder
+        const hasProfitCenter = !!value.profitCenter
+        const hasProject = !!value.project
+
+        // InternalOrder and ProfitCenter should not be provided at the same time
+        if (hasInternalOrder && hasProfitCenter) {
+          return false
+        }
+
+        // if neither provided then project is required
+        if (!hasInternalOrder && !hasProfitCenter) {
+          return hasProject
+        }
+
+        return true
+      }
+    ),
   headers: yup.object().shape({
     'api-key': yup.string().required(),
     namespace: yup.string().required(),
